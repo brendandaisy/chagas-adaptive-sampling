0d0e2801cfa8bfad4151cd294de928df39179a4f
Brendan Case <brendan.case@uvm.edu>
2021-02-05 12:46:33 -0500

    Refactering and adding correlation fig

--- stan-stuff/decay-car.R
+++ stan-stuff/decay-car.R
@@ -2,10 +2,6 @@ require(magrittr)
 require(rstan)
 require(tidyverse)
 require(bayesplot)
-require(ggthemes)
-require(RColorBrewer)
-require(gridExtra)
-require(corrr)
 
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
@@ -29,6 +25,7 @@ dm <- dm[-iso, -iso]
 dat_org <- dat_org[-iso, ]
 
 ### Code to fit model
+
 cov_mat <- make_dummy_mat(dat_org)
 
 dat_model <- list(
@@ -46,63 +43,19 @@ dat_model <- list(
 )
 
 dc_fit <- stan(
-    file = 'glm-decay-car.stan',
-    data = dat_model1,
+    file = 'decay-car.stan',
+    data = dat_model,
     chains = 4,
     iter = 7000,
     warmup = 2000,
     control = list(adapt_delta = 0.9, max_treedepth = 15)
 )
 
-### From here, either load a fit or use the one above fit
+## saveRDS(dc_fit, 'dcfit-DATE.rds')
 
-dc_fit <- readRDS('dcfit-feb3.rds')
+### Basic posterior diagnosis
 
-pairs(dc_fit, pars = c('x0', 'k', 'tau', 'phi[1]'))
-
-## how have phi correlations changed?
-phi_draws <- as.data.frame(dc_fit, pars = c('phi'))
-
-phi_cor <- cor(phi_draws) %>%
-    as_cordf %>%
-    shave %>%
-    stretch(na.rm = TRUE) %>%
-    cor_info
-
-phi_cor %>%
-    filter(dist < 500) %>%
-    ggplot(aes(x = dist, y = r, col = inf)) +
-    geom_point(size = .9) +
-    geom_vline(xintercept = median(rstan::extract(dc_fit)$x0), linetype = 'dashed') +
-    labs(y = 'Corr(i, j)', x = 'Distance (meters)', col = 'Num. Infested') +
-    theme_few()
-
-ggsave('corr-full.pdf', width = 8, height = 6)
-
-## how much autrocorrelation is left in beta?
-## TODO: consider if it is better to test each beta_p seperately
-zdraws <- as.data.frame(dc_fit, pars = c('Z_hat'))
-
-zcor <- cor(zdraws) %>%
-    as_cordf %>%
-    shave %>%
-    stretch(na.rm = TRUE) %>%
-    cor_info
-
-ggplot(zcor, aes(x = dist, y = r)) +
-    geom_point(alpha = .6) +
-    geom_smooth() +
-    theme_few()
-
-## posterior prediction: find MSE
-ydraws <- as.data.frame(sc_fit, pars = c('Y_hat'))
-
-mpe <- function(yhat) {
-    mean((yhat - dat_model$Y)^2)
-}
-
-mpe_ppd <- apply(ydraws, 1, mpe)
-summary(mpe_ppd)
+pairs(dc_fit, pars = c('alpha', 'x0', 'k', 'tau', 'phi[1]'))
 
 ## mcmc_scatter(glm_full, pars=c('alpha', 'beta[3]'))
 
@@ -148,46 +101,46 @@ summary(mpe_ppd)
 ##     theme_minimal()
 
 ## visualize network, network uncertainty
-net_parms <- as.data.frame(glm_full, pars=c('x0', 'alpha'))
+## net_parms <- as.data.frame(glm_full, pars=c('x0', 'alpha'))
 
-cdist <- c(dm[upper.tri(dm)]) %>%
-    map_dfr(~tibble(dist = .x, alpha = net_parms$alpha, x0 = net_parms$x0)) %>%
-    mutate(aC = alpha *(1 - my_logistic(dist, x0 = x0, k = .065)))
+## cdist <- c(dm[upper.tri(dm)]) %>%
+##     map_dfr(~tibble(dist = .x, alpha = net_parms$alpha, x0 = net_parms$x0)) %>%
+##     mutate(aC = alpha *(1 - my_logistic(dist, x0 = x0, k = .065)))
 
-sum_cdist <- cdist %>%
-    group_by(dist) %>%
-    summarize(maC = median(aC), laC = quantile(aC, .05), uaC = quantile(aC, .95))
+## sum_cdist <- cdist %>%
+##     group_by(dist) %>%
+##     summarize(maC = median(aC), laC = quantile(aC, .05), uaC = quantile(aC, .95))
 
-sub_cdist <- cdist %>%
-    group_by(dist) %>%
-    filter(aC > quantile(aC, .95))
+## sub_cdist <- cdist %>%
+##     group_by(dist) %>%
+##     filter(aC > quantile(aC, .95))
 
-ggplot(sum_cdist, aes(x = dist, y = maC)) +
-    geom_line() +
-    geom_ribbon(aes(ymin=laC, ymax=uaC), alpha = .3) +
-    geom_point(aes(y = aC), data = sub_cdist, alpha = .1)
+## ggplot(sum_cdist, aes(x = dist, y = maC)) +
+##     geom_line() +
+##     geom_ribbon(aes(ymin=laC, ymax=uaC), alpha = .3) +
+##     geom_point(aes(y = aC), data = sub_cdist, alpha = .1)
 
-mean_mat <- reduce2(
-    net_parms$x0,
-    net_parms$alpha,
-    ~..3 * (1 - my_logistic(dm, x0 = ..2, k = .065)) + ..1,
-    .init = 0
-) / nrow(net_parms)
+## mean_mat <- reduce2(
+##     net_parms$x0,
+##     net_parms$alpha,
+##     ~..3 * (1 - my_logistic(dm, x0 = ..2, k = .065)) + ..1,
+##     .init = 0
+## ) / nrow(net_parms)
 
-diag(mean_mat) <- 0
+## diag(mean_mat) <- 0
 
-mean_mat %>%
-    as_tibble %>%
-    mutate(sid = dat_org$id) %>%
-    pivot_longer(-sid, values_to='C', names_to = 'tid') %>%
-    ggplot(aes(x=sid, y=tid, fill=C)) +
-    geom_tile()
+## mean_mat %>%
+##     as_tibble %>%
+##     mutate(sid = dat_org$id) %>%
+##     pivot_longer(-sid, values_to='C', names_to = 'tid') %>%
+##     ggplot(aes(x=sid, y=tid, fill=C)) +
+##     geom_tile()
 
-mean_mat %>%
-    as_tibble %>%
-    mutate(sid = dat_org$id) %>%
-    pivot_longer(-sid, values_to='C', names_to = 'tid') %>%
-    mutate(dist = map2_dbl(sid, tid, ~dm[.x, .y])) %>%
-    ggplot(aes(x = dist, y = C)) +
-    geom_point() +
-    theme_minimal()
+## mean_mat %>%
+##     as_tibble %>%
+##     mutate(sid = dat_org$id) %>%
+##     pivot_longer(-sid, values_to='C', names_to = 'tid') %>%
+##     mutate(dist = map2_dbl(sid, tid, ~dm[.x, .y])) %>%
+##     ggplot(aes(x = dist, y = C)) +
+##     geom_point() +
+##     theme_minimal()
--- stan-stuff/sparse-car.R
+++ stan-stuff/sparse-car.R
@@ -2,29 +2,11 @@ require(magrittr)
 require(rstan)
 require(tidyverse)
 require(bayesplot)
-require(ggthemes)
-require(RColorBrewer)
-require(gridExtra)
-## require(MASS, exclude=c('select'))
-require(corrr)
 
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 source('../spatial-helpers.R')
 source('../covariate-helpers.R')
-
-cor_info <- function(cordf) {
-    ## append info about the pairwise neighbors onto a cordf in long form
-    n1 <- as.numeric(str_extract(cordf$x, '\\d+'))
-    n2 <- as.numeric(str_extract(cordf$y, '\\d+'))
-    
-    cordf %>%
-        mutate(
-            adj = map2_lgl(n1, n2, ~sdm[.x, .y] == 1),
-            dist = map2_dbl(n1, n2, ~am[.x, .y]),
-            inf = map2_chr(n1, n2, ~as.character(dat_model$Y[.x] + dat_model$Y[.y]))
-        )
-}
 
 village <- 'Paternito'
 cutoff <- 100
@@ -60,7 +42,7 @@ dat_model <- list(
 )
 
 sc_fit <- stan(
-    file = 'glm-sparse-car.stan',
+    file = 'sparse-car.stan',
     data = dat_model,
     chains = 4,
     iter = 7000,
@@ -68,48 +50,8 @@ sc_fit <- stan(
     control = list(max_treedepth = 15)
 )
 
-## how have phi correlations changed?
-phi_draws <- as.data.frame(sc_fit, pars = c('phi'))
+saveRDS(sc_fit, 'scfit-feb5.rds')
 
-phi_cor <- cor(phi_draws) %>%
-    as_cordf %>%
-    shave %>%
-    stretch(na.rm = TRUE) %>%
-    cor_info
+### Basic posterior diagnosis
 
-phi_cor %>%
-    filter(dist < 500) %>%
-    ggplot(aes(x = dist, y = r, col = inf)) +
-    geom_vline(xintercept = 100, linetype = 'dashed') +
-    geom_point(size = .9) +
-    labs(y = 'Corr(i, j)', x = 'Distance (meters)', col = 'Num. Infested') +
-    theme_few()
-
-ggsave('corr-sparse.pdf', width = 8, height = 6)
-
-## how much autrocorrelation is left in beta?
-## TODO: consider if it is better to test each beta_p seperately
-zdraws <- as.data.frame(sc_fit, pars = c('Z_hat'))
-
-zcor <- cor(zdraws) %>%
-    as_cordf %>%
-    shave %>%
-    stretch(na.rm = TRUE) %>%
-    cor_info
-
-ggplot(zcor, aes(x = dist, y = r)) +
-    geom_point(alpha = .6) +
-    geom_smooth() +
-    theme_few()
-
-## posterior prediction: find MSE
-ydraws <- as.data.frame(sc_fit, pars = c('Y_hat'))
-
-mpe <- function(yhat) {
-    mean((yhat - dat_model$Y)^2)
-}
-
-mpe_ppd <- apply(ydraws, 1, mpe)
-summary(mpe_ppd)
-
-
+pairs(sc_fit, pars = c('alpha', 'tau', 'phi[1]'))
