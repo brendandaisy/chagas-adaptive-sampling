ad18d6d306bc7e9081c1845abe264eee716a9cab
brendandaisy <bcase@uvm.edu>
2022-03-31 13:24:04 -0400

    remove unrelated dirs

--- old-scripts-etc/stan-stuff/diag-decay-car.R
+++
@@ -1,7 +0,0 @@
-require(rstan)
-require(bayesplot)
-require(tidyverse)
-
-dc_fit <- readRDS('stan-fits/LCARk065_2021-02-26.rds')
-
-pairs(dc_fit, pars = c('alpha', 'x0', 'tau', 'phi[1]', 'beta[3]'))
